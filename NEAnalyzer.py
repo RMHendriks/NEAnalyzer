e83ef6ec7b83598c47662885d36a3a89cfcb4ff4
RMHendriks <rm.hedriks@gmail.com>
2024-02-27 11:16:50 +0100

    Improved functionality

--- NEAnalyzer.py
+++ NEAnalyzer.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from seaborn import displot, barplot
+from numpy import transpose
+from copy import deepcopy
+from collections import Counter
 from typing import Union
 
 
@@ -22,18 +25,19 @@ class NEA():
         """
 
         self.A = A
+
+        # TODO tmp solution
+        self.F = F
+        self.T = T
+        self.L = L
+
         self.named_entity = input_list
         self.named_entity_list = self.get_named_entity(input_list)
 
         if not self.named_entity_list:
             print("Entity list is empty")
         else:
-            print("Succes!")
-
-        # TODO tmp solution
-        self.F = F
-        self.T = T
-        self.L = L
+            print("Success!")
 
         self.pd_data_frame = self.build_data_frame()
     
@@ -46,31 +50,40 @@ class NEA():
 
         node_list: list[list[tuple[int]]] = []
 
-        # convert a str list
-        if input_list and all(isinstance(s, str) for s in input_list):
+        for element in input_list:
+            
+            # convert a str list
+            if element and isinstance(element, str):
 
-            for s in input_list:
-                query = f"""
+                query_search_list = []
+
+                for char in element:
+                    if char == " ":
+                        query_search_list.append("\\ ")
+                    else:
+                        query_search_list.append(char)
+
+                query_search = ''.join(query_search_list)
+
+            # convert an int (node) list
+            # TODO node inputs need more checks if valid search all other named entities
+            elif element and isinstance(element, int) and self.F.entityId.v(element) is not None:
+                query_search = self.T.text(element)
+
+            else:
+                print(f"Input \"{element}\" is not valid")
+                continue
+
+            query = f"""
 letter
-    word trans~{s} entityId entityKind*
-        """
-                result = self.A.search(query)
-                if not result:
-                    print(f"\"{s}\" does not exist as a named entity")
-                else:
-                    node_list.append(result)
-
-        # convert an int (node) list
-        # TODO node inputs need more checks if valid
-        elif input_list and all(isinstance(i, int) for i in input_list):
-            for i in node_list:
-                if self.F.entityId.v(i) is not None:
-                    letter = self.L.u(i, otype="letter")
-                    self.node_list.append([i, letter])
-        
-        else:
-            print("Input list is not valid")
-        
+    word trans~{query_search} entityId entityKind*
+                    """
+            result = self.A.search(query)
+            if not result:
+                print(f"\"{element}\" does not exist as a named entity")
+            else:
+                node_list.append(result)
+    
         
         return node_list
     
@@ -113,36 +126,139 @@ letter
             data_frame_list.append(pd.DataFrame(named_entity_dict))
 
         return data_frame_list
+    
+    def normalize_corpus(self) -> dict[int, float]:
+        """ 
+        Calculates the amount of words in the corpus per year and give back a dict
+        of ratios per year that can be used to normalize the corpus.
+        """
 
-    def draw_plot(self, use_fixed_x=False) -> None:
+        # Sort the letters by year in a dict
+        letter_by_year_dict = {}
+
+        for letter in self.F.otype.s("letter"):
+            year = self.F.year.v(letter)
+            words = len(self.L.d(letter, otype="word"))
+            if year in letter_by_year_dict:
+                letter_by_year_dict[year] += words
+            elif year is None:
+                continue
+            else:
+                letter_by_year_dict[year] = words
+
+        # Normalize the years
+        noralized_year_dict = {}
+        total_words = sum(letter_by_year_dict.values())
+
+        for year, words in letter_by_year_dict.items():
+            noralized_year_dict[year] = words / total_words
+
+        return noralized_year_dict
+
+    def draw_plot(self, use_fixed_x=False, normalize=False) -> None:
         """
         Draws a displot in Jupyter Notebooks of the occurences by date.
-        TODO Add an option to use a fixed x range
         """
 
         FIXED_X_AXIS = (1610, 1767)
 
-        data: list[pd.DataFrame] = []
+        data = []
 
         for data_frame in self.pd_data_frame:
             year_list = data_frame["Year"]
             year_list.name = data_frame["Name"][0]
             data.append(year_list)
 
+        if normalize:
+            normalized_year_dict = self.normalize_corpus()
+            tmp_data_dict = []
+
+            for year_list in data:
+                tmp_data_dict.append(Counter(year_list))
+
+            data = []
+
+            for year_dict_list in tmp_data_dict:
+                for year in year_dict_list:
+                    year_dict_list[year] *= normalized_year_dict[year]
+
+                data.append(pd.DataFrame(year_dict_list.items(), columns=['year', 'value']))
+
+            print(data)
+
         if use_fixed_x:
             displot(data=data, multiple='stack')
             plt.xlim(FIXED_X_AXIS)
         else:
-            displot(data=data, x="Year", multiple='stack')
+            displot(data=data, multiple='stack')
 
         plt.show()
 
-    def detect_overlap(self) -> None:
+    def detect_overlap_index(self) -> None:
         """
-        TODO Detect if NE overlap in the same letter
+        TODO See if it is possible to detect overlap on more than 2 nodes (x nodes of overlap)
         """
 
-        pass
+        overlap_list: list[list(int, int, int)] = []
+
+        for letter_node_list in self.named_entity_list:
+            for letter, node in letter_node_list:
+                for lst in self.named_entity_list:
+                    if lst == letter_node_list:
+                        continue
+                    for letter2, node2 in lst:
+                        if letter == letter2 and [letter, node2, node] not in overlap_list:
+                            overlap_list.append([letter, node, node2])
+
+        # Shuffles the nodes back into order
+        for overlap in overlap_list:
+            if overlap[1] > overlap[2]:
+                overlap[1], overlap[2] = overlap[2], overlap[1]
+
+        self.print_overlap(overlap_list)
+
+    def detect_overlap_letter(self, print_letters=False) -> None:
+        """ Detects overlap of named entities in a letter"""
+
+        copied_named_entities_list = deepcopy(self.named_entity_list)
+        letter_list: list(tuple(int)) = []
+
+        for named_entity in copied_named_entities_list:
+            letter_list.append(tuple(transpose(named_entity)[0]))
+
+        # uses the first list of letters as a base to compare to
+        intersecting_elements = set(letter_list[0])
+
+        # itarates over the remaining letter lists to see if any intersections remain
+        for lst in letter_list[1:]:
+            intersecting_elements.intersection_update(lst)
+
+        print(f"Overlap of the {len(self.named_entity)} named entities in {len(intersecting_elements)} letters.\n")
+
+        if print_letters:
+            for letter in intersecting_elements:
+                self.A.plain(letter)
+
+    def print_overlap(self, overlap_list) -> None:
+        """
+        Prints overlap information
+        TODO redesign the information shown after starting work on the showing of better
+        formated text 
+        TODO Highlight the overlap in the letter if shown
+        """
+
+        print(f"{len(overlap_list)} overlap(s)")
+
+        for n, overlap in enumerate(overlap_list):
+            print(f"Overlap {n + 1}: ", end="")
+            for node in overlap[1:]:
+                print(self.T.text(node), end="")
+                if node != overlap[-1]:
+                    print("- ", end="")
+            self.A.plain(overlap[0])
+            for node in overlap[1:]:
+                self.A.plain(node)
+                self.A.plain(self.L.u(node, otype="line")[0])
 
     def print_info(self) -> None:
         """
